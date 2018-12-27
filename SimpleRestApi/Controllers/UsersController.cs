a012ff48e2a98695bb89ab5a2019e9fc746f2071
Dmitriy Blokhin <Dmitriy_Blokhin@epam.com>
2018-12-27 21:36:29 +0400

    Initial implementation of Rest API

---
+++ SimpleRestApi/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using SimpleRestApi.Models;
+
+namespace SimpleRestApi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/users")]
+    public class UsersController : ApiController
+    {
+        private readonly User[] _users = {
+            new User {Id = 1, Login = "Ivan1", Name = "Ivan", SurName = "Bobrov", Email = "Ivan.Bobrov@mail.net"},
+            new User {Id = 2, Login = "Petr1", Name = "Petr", SurName = "Ivanov", Email = "Petr.Ivanov@mail.net"},
+            new User {Id = 3, Login = "Roman1", Name = "Roman", SurName = "Sidorov", Email = "Roman.Sidorov@mail.net"}
+        };
+
+        // GET: api/Users
+        [Route("")]
+        public IEnumerable<User> GetUsers()
+        {
+            return _users;
+        }
+
+        // GET: api/Users/5
+        [Route("{id:int}")]
+        [ResponseType(typeof(User))]
+        public IHttpActionResult GetUser(int id)
+        {
+            var user = _users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+    }
+}
---
+++ SimpleRestApi/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleRestApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Name { get; set; }
+
+        public string SurName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
